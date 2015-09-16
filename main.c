ded27174bf3f70e1c4aa4f9ef7f984682c0c2b54
August M <augustmbius@gmail.com>
2015-09-16 11:59:32 +0200

    ECG file

--- main.c
+++ main.c
@@ -5,15 +5,27 @@
 int main(){
 	static const char filename[] = "ECG.txt";
 	FILE *file = fopen(filename,"r"); //Reads file into variable file.
-	int i = 1;
+	int i = 0;
 	int fileLength = 10000; //Number of data points in ECG.txt
+	int a[32];
+	int value;
 
-	while(i < 1000){
-		printf("%d\n",getNextData(file));
+
+
+	while(i < 32){
+		value = getNextData(file);
+		a[i] = value;
+		printf("Pikoghor %d\n",value);
 		usleep(4*1000); // This only works for unix systems.
 		i++;
+
 	}
 
+	/*
+	for(int i = 0; i< 32; i++){
+		printf("%d\n",a[i]);
+	}
+	*/
 
 	return 0;
 }
